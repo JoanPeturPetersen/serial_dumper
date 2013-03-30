c8d2de569ec85511cf276aaf349cddcf112d0166
Jóan Petur Petersen <joanpeturpetersen@gmail.com>
2013-03-30 14:19:29 +0100

    First version.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 serial_dumper
 =============
 
-Serial port dumper
+Dump data received from a serial port to standard out.
+
+It is possible to specify baudrate, stop bits, parity, and byte size.
+
+Requirements
+============
+
+Requires pySerial and docopt. If you have pip installed, you can simply
+write:
+
+  pip install pyserial docopt
+
---
+++ serial_dumper.py
@@ -0,0 +1,43 @@
+"""
+Usage: serial_dumper.py [--baudrate=BAUD] [--bytesize=BYTESIZE]
+                        [--parity=PARITY] [--stopbits=STOPBITS] PORT
+
+Options:
+  -h, --help           show this help message and exit
+  --baudrate=BAUD      Specify baud rate [default: 9600]
+  --bytesize=BYTESIZE  Specify byte size [default: 8]
+  --parity=PARITY      Specify parity bit, one of [N, E, O] [default: N]
+  --stopbits=STOPBITS  Specify stop bits [1, 2, 1.5] [default: 1]
+
+Example:
+  serial_dumper --baudrate=4800 /dev/ttyr01
+"""
+
+from docopt import docopt
+import serial
+import sys
+
+
+def dump_serial(ser_port, ser_baudrate, ser_bytesize, ser_parity,
+        ser_stopbits):
+    """
+    """
+    ser = serial.Serial(port=ser_port, baudrate=ser_baudrate,
+        bytesize=ser_bytesize, parity=ser_parity, stopbits=ser_stopbits)
+    try:
+        while True:
+            sys.stdout.write(ser.readline())
+    except Exception, e:
+        print "Communication problems: ", e
+    try:
+        ser.close()
+    except:
+        pass
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Serial dumper 1.0')
+    print arguments
+    dump_serial(arguments['PORT'], ser_baudrate=arguments['--baudrate'],
+        ser_bytesize=int(arguments['--bytesize']),
+        ser_parity=arguments['--parity'],
+        ser_stopbits=int(arguments['--stopbits']))
